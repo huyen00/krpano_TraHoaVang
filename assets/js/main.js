b1808fb8a7b4e7499cf9ed557ea9448c5bdd2a52
Hoàng Thị Huyền <huyenht@VNIMATION.COM>
2022-08-19 18:19:52 +0700

    fix giao dien popup option chu Thanh

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,6 +177,9 @@ $(document).ready(function(){
       $("#icon_info").css('opacity','0');
       $("#icon_map").css('opacity','0');
   });
+// xử lý popup
+
+
   // xử lý menu icon sub
   $(".menu_sub").hide();
   $("#icon_menusub").click(function(){
@@ -224,6 +227,181 @@ $(document).ready(function(){
 
 });
 
+var array_list = [
+  // 0
+  {
+    title_list : "Thông tin khu nguyên liệu A",
+    content_list: "Là khu nguyên liệu đã  và đang cho thu hoạch ổn định (gồm nhiều cây 3-7 năm tuổi, ngoài ra có các khu vực đang phát triển cây con độ tuổi 1,5 - 2,5 năm tuổi). Diện tích khoảng 10ha"
+   
+    
+  },
+  // 1
+  {
+    title_list : "Thông tin khu nguyên liệu B",
+    content_list: "Là khu mới phát triển (gồm nhiều cây con độ tuổi dưới 1 năm tới 2 năm tuổi), đây là khu nguyên liệu của thành viên HTX, mà trong đó thành viên được hỗ trợ giống, kỹ thuật, bao tiêu thu mua... tạo vùng nguyên liệu ổn định lâu dài cũng như tạo công ăn việc làm cho nhân dân địa phương. Diện tích khoảng 6ha."
+   
+  
+  },
+  // 2
+  {
+    title_list : "Thông tin khu cây giống lâu năm",
+    content_list: "Khu gồm cây trà mẹ, lâu năm tuổi (cây 10-20 năm tuổi) vừa cung cấp hoa vừa cung cấp quả, hạt giống cũng như phôi chiết. Khu cây lâu năm tuổi kết hợp một số khu vực khu nguyên liệu A là nguồn cung cấp chính cây con cho cả khu vực nguyên liệu xung quanh khác."
+   
+   
+  
+  },
+  // 3
+  {
+    title_list : "Thông tin khu Vườn Ươm",
+    content_list: "Khu vườn ươm là nơi nhân giống, tạo sự chủ động nguồn cung cho HTX cũng như vùng nguyên liệu được bền vững."
+   
+   
+  
+  },
+  // 4
+  {
+    title_list : "Thông tin Nhà Trưng bày sản phẩm",
+    content_list: "Khu tổ hợp nhà trưng bày sản phẩm gồm khu trưng bày, khu xưởng sấy, kho và tiếp giáp khu vườn ươm.Đây là điểm giới thiệu và bán sản phẩm OCOP thứ 3/5 điểm giới thiệu và bán sản phẩm OCOP trên địa bàn tỉnh được hỗ trợ theo QĐ 1656/QĐ-UBND ngày 19 tháng 8 năm 2020 của UBND tỉnh đã khai trương và đi vào hoạt động "
+    
+   
+  
+  },
+  // 5
+  {
+    title_list : "Thông tin Nhà thu mua",
+    content_list: "Nhà ở của chủ cơ sở kinh doanh kèm chức năng thu mua nguyên liệu của thành viên HTX cũng như nhân dân quanh vùng"
+  },
+  // 6
+  {
+    title_list : "",
+    content_list: "Đây là đường 293 (hướng đi về chùa Tây Yên Tử - chùa Hạ) tuyến đường Hoằng dương phật pháp kết nối địa danh tâm linh, lịch sử giữa thành phố Bắc Giang và vùng đất Tây Yên Tử"
+  },
+  // 7
+  {
+    title_list : "",
+    content_list: "Đây là đường 293 (hướng đi về tp Bắc Giang) tuyến đường Hoằng dương phật pháp kết nối địa danh tâm linh, lịch sử giữa thành phố Bắc Giang và vùng đất Tây Yên Tử"
+  },
+  // 8
+  {
+    title_list : "Cây trà 6 năm tuổi",
+    content_list: "Cây đang thu hoạch và có thể là cây giống bố mẹ Cây cho thu hoạch cả hoa và lá Do đặc tính cây ưa bóng râm nên có thể trồng xen canh bên cạnh cây vải (đặc sản địa phương đang có), nhã... mà không phải phá cây trồng cũ đang có trên mặt bằng."
+  },
+  // 9
+  {
+    title_list : "Cây trà 6 năm tuổi",
+    content_list: "Cây đang thu hoạch và có thể là cây giống bố mẹ Cây cho thu hoạch cả hoa và lá Do đặc tính cây ưa bóng râm nên có thể trồng xen canh bên cạnh cây vải (đặc sản địa phương đang có), nhã... mà không phải phá cây trồng cũ đang có trên mặt bằng."
+  },
+  // 10
+  {
+    title_list : "Cây trà gần 1 tuổi",
+    content_list: "Cây mới được trồng bên khu vườn mới của thành viên HTX Do đặc tính cây ưa bóng râm nên có thể trồng xen canh bên cạnh cây vải (đặc sản địa phương đang có), nhã... mà không phải phá cây trồng cũ đang có trên mặt bằng."
+  },
+  // 11
+  {
+    title_list : "Dược liệu khác",
+    content_list: "Đây là mặt hàng cũng nằm trong danh mục được kiểm định và được có mặt trong gian hàng"
+  },
+  // 12
+  {
+    title_list : "Thông tin máy sấy lạnh",
+    content_list: "Sấy lạnh là phương pháp sấy bằng tác nhân là không khí rất khô ở nhiệt độ thấp hơn nhiệt độ sấy thông thường. Dải nhiệt độ sấy từ 35-600C, độ ẩm không khí sấy vào khoảng 10-30%. Quá trình sấy được tiến hành ở áp suất khí quyển. Máy sấy lạnh được cấu thành bởi một máy bơm nhiệt được đặt trong một tủ sấy hoặc một hầm sấy tùy theo quy mô. Máy bơm nhiệt có một đầu nóng và một đầu lạnh. Đầu nóng sẽ cung cấp nhiệt lượng cho tác nhân sấy, còn đầu lạnh dùng để tách ẩm cho không khí sấy. Do đó, máy sấy lạnh còn gọi là máy sấy bơm nhiệt. "
+  },
+  // 13
+  {
+    title_list : "Thông tin máy sấy thăng hoa",
+    content_list: "Sấy thăng hoa hay sấy đông khô: Đây là phương pháp sấy ưu việt nhất trong tất cả phương pháp sấy hiện nay. Sấy thăng hoa (freeze drying) là một kỹ thuật còn được gọi là “làm khô lạnh”. Trong đó sản phẩm được sấy khô qua quá trình thăng hoa của nước dưới nhiệt độ và áp suất thấp. Nước trong sản phẩm được đông lạnh thành đá sau đó được loại bỏ trực tiếp ra khỏi sản phẩm bằng cách hóa hơi. Điều này được thực hiện trong môi trường chân không."
+  },
+
+  
+];
+
+
+var array_info = [
+  {
+    title_info: "Thông tin sản phẩm Trà Loại 1",
+    content_info: "Trà loại 1 là ....<br> 1...<br> 2.... <br>3..."
+   
+    
+  },
+  {
+    title_info : "Thông tin sản phẩm Trà Loại 2",
+    content_info: "Trà loại 2 là ....<br> 1...<br> 2.... <br>3..."
+   
+  
+  },
+  {
+    title_info : "Thông tin sản phẩm Trà Loại 3",
+    content_info: "Trà loại 3 là ....<br> 1...<br> 2.... <br>3..."
+   
+   
+  
+  },
+  {
+    title_info : "Giới thiệu về HTX Lựu Chanh",
+    content_info: "- Loại hình doanh nghiệp: Hợp Tác Xã<br> - Người đại diện: Hà Thị Chanh<br>- Địa chỉ: Thôn Nhân Lý, xã Trường Sơn, huyện  Lục Nam, tỉnh Bắc Giang<br>- Điện thoại: 0962.831.385<br>- Email: duoclieutamduc@gmail.com<br>Hợp tác xã sản xuất cây dược liệu Lựu Chanh được thành lập đầu năm 2017 với ngành nghề chính là trồng và chế biến sản xuất dược liệu mà trong đó trà hoa vàng là sản phẩm chủ lực chính, sản phẩm được công nhận và xếp loại OCOP 4* sao.<br>Với sự cố gắng vượt qua rất nhiều khó khăn trở ngại cũng như không ngừng học hỏi áp dụng những biện pháp nâng cao hiệu quả, hiện nay, tổng diện tích trồng cây chè hoa vàng của hợp tác xã khoảng trên 10ha, có vườn ươn cây giống để cung cấp cho các địa phương.<br>Ngày 19/12/2020 tại thôn Nhân Lý, xã Trường Sơn, huyện Lục Nam, Hợp tác xã sản xuất cây dược liệu Lựu Chanh xã Trường Sơn long trọng tổ chức buổi khai trương điểm giới thiệu và bán sản phẩm OCOP và các đặc sản vùng miền của tỉnh Bắc Giang.<br>Với vị trí rất thuận lợi nằm trên con đường (Đường 293) tuyến đường Hoằng dương phật pháp kết nối những địa danh tâm linh, lịch sử giữa thành phố Bắc Giang và vùng đất Tây Yên Tử linh thiêng, huyền bí. <br>Điểm giới thiệu và bán sản phẩm OCOP (của HTX sản xuất cây dược liệu Lựu Chanh xã Trường Sơn ) được khai trương hứa hẹn sẽ tạo ra chuỗi liên kết giữa các chủ thể sản xuất và nhà phân phối sản phẩm OCOP, góp phần quan trọng trong việc giới thiệu, quảng bá các sản phẩm OCOP của tỉnh nói chung và sản phẩm OCOP của huyện Lục Nam nói riêng; đồng thời, là điểm đến để nhân dân trong tỉnh và du khách có thể đến tham quan, mua sắm các sản phẩm hàng hoá chủ lực, đặc trưng của các vùng miền trong và ngoài tỉnh, đáp ứng các tiêu chuẩn về quy trình sản xuất, mẫu mã bao bì, xuất xứ nguồn gốc và chất lượng sản phẩm OCOP.<br>Đây là điểm giới thiệu và bán sản phẩm OCOP thứ 3/5 điểm giới thiệu và bán sản phẩm OCOP trên địa bàn tỉnh được hỗ trợ theo QĐ 1656/QĐ-UBND ngày 19 tháng 8 năm 2020 của UBND tỉnh đã khai trương và đi vào hoạt động (sau điểm giới thiệu và bán sản phẩm OCOP FRESH FOOD tại 454  Lê Lợi, TP Bắc Giang; điểm trưng bày và bán sản phẩm OCOP tại thị trấn Bích Động huyện Việt Yên)."
+   
+   
+  
+  },
+  {
+    title_info : "Giới Thiệu về cây Trà Hoa Vàng",
+    content_info: "Trà hoa vàng hay còn gọi là Kim hoa trà, Chè rừng, Hải đường vàng, là một cây có giá trị làm cảnh và là loại thảo dược quý. Cây có nguồn gốc bản địa phân bố ở một vài vùng núi phía bắc nước ta. Theo nghiên cứu khoa học hiện đại, Trà hoa vàng là loài dược liệu rất quý, trà có chứa hơn 400 hoạt chất và trong đó quý giá là các saponin, tea polyphenol và các nguyên tố như Selenium (Se), Germannium (Ge), Kalium (K), Kẽm (Zn), Molypden (Mo), Vanadium (V), Mangan (Mn) và các vitamin B1, B2, C. Trà Hoa Vàng được giới y học nghiên cứu rất nhiều và đã được ứng dụng trên nhiều quốc gia như Mỹ,Trung Quốc, Pháp, Nhật Bản, Việt Nam ... <br> Nước sắc lá trà có tác dụng hạ huyết áp rõ ràng và tác dụng được duy trì trong thời gian tương đối dài. Trong lá trà có những hoạt chất làm giảm tổng hàm lượng lipit trong huyếtthanh máu, giảm lượng cholesterol mật độ thấp (cholesterol xấu) và tăng lượng cholesterol mật độ cao (cholesterol tốt). Nước sắc lá trà có tác dụng ức chế sự tụ tập của tiểu cầu, chống sự hình thành huyết khối gây tắc nghẽn mạch máu. Phòng ngừa ung bướu và ức chế sự phát triển của các khối u khác. Giải độc gan và thận, ngăn ngừa xơ vữa động mạnh máu. Ức chế và tiêu diệt vi khuẩn. Ngoài ra, lá chè còn có tác dụng chống viêm, chống dị ứng và duy trì trạng thái bình thường của tuyến giáp. Cây Trà Hoa Vàng ngày nay rất được nhiều người ưa chuộng trồng để làm cảnh và dùng làm trà như một loại thảo dược dùng để dưỡng sinh.<br> 2Trà hoa vàng của Hợp tác xã đã đạt chuẩn OCOP 4 sao và là sản phẩm tiêu biểu cấp Tỉnh 2019, cấp khu vực 2020 và toàn quốc 2021. <br>3Sản phẩm được trồng hữu cơ không dùng thuốc bảo vệ thực vật và  phân bón hoá học. Mỗi năm chỉ ra hoa 1 lần vào tháng 10 đến tháng 12 âm lịch hàng năm. Thu hoạch vào buổi sáng để giữ được hoạt chất quý của trà không bị ảnh hưởng bởi ánh nằng mặt trời. Không thu hái vào trời mưa để thu được những bông trà thơm ngon nhất. Trà được chế biến bằng quy trình sấy công nghệ cao, hiện đại nhất hiện tại sấy thăng hoa, trà khô ở nhiệt độ -40 độ C để đảm bảo giữ được nguyên dược tính hoạt chất quý của trà."
+   
+   
+  
+  },
+  
+];
+
+// xử lý popup
+
+function showform(data){
+ 
+  document.getElementById('popup_').style.display = "flex";
+
+  $("#title_form").html(data.title_list);
+  $("#content_form").html(data.content_list);
+}
+// show info tra
+function show_info(data){
+  document.getElementById('info_information').style.display = "flex";
+  $("#title_info_general").html(data.title_info);
+  $("#content_info_general").html(data.content_info);
+  
+  
+}
+function show_image(){
+  document.getElementById('image_tra').style.display = "flex";
+
+   document.getElementById('info_information').style.display = "none";
+}
+function close_info_tra(){
+  document.getElementById('image_tra').style.display = "none";
+  document.getElementById('info_information').style.display = "flex";
+}
+function close_info(){
+  document.getElementById('info_information').style.display = "none";
+}
+function show_htx(data){
+  document.getElementById('info_duan').style.display = "flex";
+ 
+ 
+  $("#title_info_duan").html(data.title_info);
+  $("#content_info_duan").html(data.content_info);
+}
+function show_image_duan(){
+  document.getElementById('info_infomation_image').style.display = "flex";
+  document.getElementById('info_duan').style.display = "none";
+}
+
+function close_image_duan(){
+  document.getElementById('info_infomation_image').style.display = "none";
+  document.getElementById('info_duan').style.display = "flex";
+}
+function close_duan(){
+  document.getElementById('info_duan').style.display = "none";
+  $(".info").css("display","block");
+}
 // xử lý voice 
 (function ($) {
   embedpano({
@@ -231,6 +409,10 @@ $(document).ready(function(){
       target: "pano",
       html5: "only",
       mobilescale: 1.0,
+      initvars: {
+        array_list_xml: array_list,
+        array_info_xml:array_info,
+      },
       passQueryParameters: true,
       onready: krpanoReady
   });
@@ -263,7 +445,20 @@ $(document).ready(function(){
     // xu ly VR
     $("#icon_VR").on("click", function(e){
       krpano.call("webvr.enterVR()");
-    })
+    });
+    // xử lý tắt bật xoay
+    $("#icon_unrotate").hide();
+    $("#icon_rotate").on("click",function(e){
+      $("#icon_unrotate").show();
+      $("#icon_rotate").hide();
+      krpano.call("toggleRotate()");
+    });
+    $("#icon_unrotate").on("click",function(e){
+      $("#icon_unrotate").hide();
+      $("#icon_rotate").show();
+      krpano.call("toggleRotate()");
+    });
+
     // xu ly scene
     
     $("#icon_back").on("click",function(e){
@@ -461,12 +656,51 @@ responsive: [
 // xư lý thumbs list
 $(document).ready(function(){
   $('.slick-slide').removeClass('slick-cloned');
-$(".icon_thumbs_up").click(function(){
-  $(".list-thumbs").toggleClass('list-thumbs-active');
-  $(".icon_thumbs_up").toggleClass('icon_thumbs_down');
+  $(".icon_thumbs_up").click(function(){
+   
+
+    $(".list-thumbs").toggleClass('list-thumbs-active');
+    $(".icon_thumbs_up").toggleClass('icon_thumbs_down');
  
+  });
+  // xuwr lys click close popup
+ 
+  $(".button_close").click(function(){
+    $(".popup").hide();
+  });
+  
+  // $("#close_info").click(function(){
+  //   $(".info").hide();
+  // });
+
+
+  // $("#info_htx").click(function(){
+  //   $("#info_information").css("display","flex");
+    
+  // });
+  // click close_general thif close info_information, show info
+  // $("#close_general").click(function(){
+  //   $("#info_information").css("display","none");
+  //   $(".info").css("display","block");
+  // });
+
+
+
+  // click image_general thì show info_infomation_image, hide info_ìnormation
+  // $("#image_general").click(function(){
+  //   $("#info_infomation_image").css("display","flex");
+  //   $("#info_information").css("display","none");
+  // });
+
+  // // click close_image thif close info_infomation_image, show image_general
+  // $("#close_image").click(function(){
+  //   $("#info_infomation_image").css("display","none");
+  //   $("#info_information").css("display","flex");
+  // });
+  
+  
 });
-});
+
 
 // xu ly minimap
 $(".panWrapper").mousedown(function(e){
@@ -490,4 +724,6 @@ $('.list-thumbs').on('afterChange', function(event, slick, currentSlide){
 $('.list-thumbs').slick();
 
 });
+
+// xu lý showe info 
 
